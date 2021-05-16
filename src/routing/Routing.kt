4908f12e59722ebf77d1c8e293b611c1a12b1690
Chesire <troyrijkaard@hotmail.co.uk>
2021-05-16 20:57:50 +0100

    feat: get the initial server up and running with hello world
    Get an initial implementation ready that starts up the ktor server, and upon hitting the url serves
    up Hello World.

---
+++ src/routing/Routing.kt
@@ -0,0 +1,15 @@
+package com.chesire.routing
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun Application.configureRouting(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
