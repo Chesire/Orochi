5000d38e69ebd911c5b7d0303dce709f3f46f875
Chesire <troyrijkaard@hotmail.co.uk>
2021-11-07 18:16:54 +0000

    feat: add status endpoint
    Add endpoint for getting the services status.

--- src/routing/Routing.kt
+++ src/routing/Routing.kt
@@ -1,16 +1,12 @@
 package com.chesire.routing
 
+import com.chesire.routing.status.status
 import io.ktor.application.Application
-import io.ktor.application.call
-import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.routing
 
 @Suppress("UnusedPrivateMember")
-fun Application.configureRouting(testing: Boolean = false) {
+fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        status()
     }
 }
