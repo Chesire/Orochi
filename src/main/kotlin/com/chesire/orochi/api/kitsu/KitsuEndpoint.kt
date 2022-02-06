4f07d5e6f8b590cd1c287097003f3dfe86ebdc7a
Chesire <troyrijkaard@hotmail.co.uk>
2022-02-06 16:34:47 +0000

    feat: begin adding the calls required to retrieve the users library
    Add the skeleton for the request and some comments on how to go about pulling the users library.

--- src/main/kotlin/com/chesire/orochi/api/kitsu/KitsuEndpoint.kt
+++ src/main/kotlin/com/chesire/orochi/api/kitsu/KitsuEndpoint.kt
@@ -1,6 +1,10 @@
 package com.chesire.orochi.api.kitsu
 
 object KitsuEndpoint {
+    object Library {
+        const val Entries = "https://kitsu.io/api/edge/library-entries"
+    }
+
     object OAuth {
         const val Token = "https://kitsu.io/api/oauth/token"
     }
--- src/main/kotlin/com/chesire/orochi/routes/library/kitsu/KitsuLibraryService.kt
+++ src/main/kotlin/com/chesire/orochi/routes/library/kitsu/KitsuLibraryService.kt
@@ -1,8 +1,17 @@
 package com.chesire.orochi.routes.library.kitsu
 
+import com.chesire.orochi.api.kitsu.KitsuEndpoint
+import com.chesire.orochi.ext.isSuccessful
+import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
 import io.ktor.client.HttpClient
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 /**
  * Provides service to interact with Kitsu library.
@@ -10,6 +19,34 @@ import io.ktor.client.HttpClient
 class KitsuLibraryService(private val client: HttpClient) {
 
     suspend fun retrieveLibrary(authHeader: String?): Result<Unit, Unit> {
-        return Ok(Unit)
+        // TODO: Introduce builder for the URL, since we need various filters such as userId
+        val response = client.get<HttpResponse>(KitsuEndpoint.Library.Entries) {
+            accept(ContentType.Application.Json) // application/vnd.api+json
+            contentType(ContentType.Application.Json) // application/vnd.api+json
+            header("Authorization", "Bearer $authHeader") // Get this from somewhere else
+        }
+
+        return if (response.isSuccessful) {
+            Ok(Unit)
+        } else {
+            Err(Unit)
+        }
     }
 }
+
+/*
+Kitsu error object
+{
+  error: 'invalid_request',
+  error_description: '<reason_why>'
+}
+
+Kitsu error codes
+Error	Status	Explanation
+invalid_request	400	The request is missing a parameter, uses an unsupported parameter or repeats a parameter.
+invalid_client	401	The request contains an invalid client ID or secret.
+invalid_grant	400	The authorization code (or password with the password grant) is invalid or expired.
+invalid_scope	400	The request contains an invalid scope (password or client credential grants).
+unauthorized_client	400	The client is not authorized to use the requested grant type.
+unsupported_grant_type	400	The grant type requested is not recognized by the server.
+ */
